2ec48a1a5713e43d450564cefe429697ce5bb278
Anatoli Grishenko <Anatoli.Grishenko@gmail.com>
2022-02-14 12:36:07 +0100

    Primera etapa de refactoring a Ole2

--- src/deathstar/MyDeathStar.java
+++ src/deathstar/MyDeathStar.java
@@ -1,6 +1,7 @@
 package deathstar;
 
 import agents.DeathStar;
+import agents.DeathStarDeLuxe;
 import appboot.LARVABoot;
 import static crypto.Keygen.getHexaKey;
 
@@ -9,6 +10,7 @@ public class MyDeathStar {
     public static void main(String[] args) {
         LARVABoot connection = new LARVABoot();
 
+//        connection.Boot("isg2.ugr.es",1099);
         connection.Boot("localhost",1099);
         connection.launchAgent("DEATHSTAR "+getHexaKey(8), DeathStar.class);
         connection.WaitToClose().ShutDown();
